fee9d5ede2e7228952cddb77c7ab3ff0b1b7c1e8
ZhaoYuhao <2249011834@qq.com>
2024-03-23 10:27:32 +0800

    初始化地球的样式，设置了场景的模式

--- src/views/Earth/index.vue
+++ src/views/Earth/index.vue
@@ -3,6 +3,7 @@
     </div>
 </template>
 <script>
+import * as Cesium from "@cesium/engine"
 import { Cartesian3, createOsmBuildingsAsync, Ion, Math as CesiumMath, Terrain, Viewer, CzmlDataSource } from "cesium";
 import { mapGetters, mapActions } from "vuex"
 import "cesium/Build/Cesium/Widgets/widgets.css";
@@ -14,7 +15,7 @@ window.CESIUM_BASE_URL = '/node_modules/cesium/Build/Cesium';
 export default {
     data() {
         return {
-            viewer:undefined,
+            viewer: undefined,
         }
     },
     mounted() {
@@ -22,7 +23,45 @@ export default {
         Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI1NDBkN2Y0Yy1jNzAxLTQ4MzgtYTQ2MC0zODdiN2Q3NmFhZTgiLCJpZCI6MTgwMzg4LCJpYXQiOjE3MDA3OTYzODh9.5CecpNsUZIzxfJEFK6N831bB939oqdbx3YRN34IZ31A';
 
         // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
-        this.viewer = new Viewer('cesiumContainer');
+        this.viewer = new Viewer('cesiumContainer', {
+            shouldAnimate: true,
+            infoBox: true, // 是否显示点击要素之后显示的信息
+            // 去掉地球表面的大气效果黑圈问题
+            orderIndependentTranslucency: true,
+
+            contextOptions: {
+                webgl: {
+                    alpha: true,
+                },
+            },
+            // 时间线
+            timeline: true,
+            // 时间加速盘
+            animation: true,
+            // -------------
+            baseLayer: this.createImageryLayer('./data/cesium-assets/imagery/NaturalEarthII'),
+            baseLayerPicker: false,
+            geocoder: false,
+            fullscreenButton: false,
+            homeButton: false,
+            navigationHelpButton: false,
+            navigationInstructionsInitiallyVisible: false,
+            sceneModePicker: false,
+            selectionIndicator: false,
+        });
+
+        // Cesium default settings
+        // 设置自动播放时钟
+        this.viewer.clock.shouldAnimate = true;
+        // 启用地球场景的照明效果
+        this.viewer.scene.globe.enableLighting = true;
+        // 增强场景的关照和颜色的动态范围
+        this.viewer.scene.highDynamicRange = true;
+        // 指定了场景每秒的最大渲染时间变化
+        this.viewer.scene.maximumRenderTimeChange = 1 / 30;
+        // 启用了请求渲染模式，允许场景在必要时请求进行渲染
+        this.viewer.scene.requestRenderMode = true;
+
         console.log("finished...")
         console.log("wdInfoMap...", wdInfoMap)
         // 初始化windowSize,存入store
@@ -45,6 +84,20 @@ export default {
                     this.viewer.dataSources.add(dronePromise);
                 });
             }, 0)
+        },
+        // 加载地球图层(data/cesium-assets/imagery/NaturalEarthII)
+        createImageryLayer(url) {
+            const provider = {
+                create: () => Cesium.TileMapServiceImageryProvider.fromUrl(url, {
+                    maximumLevel: 5,
+                    credit: "Imagery courtesy Natural Earth",
+                }),
+                alpha: 1,
+                base: true,
+            }
+            const layer = Cesium.ImageryLayer.fromProviderAsync(provider.create())
+            layer.alpha = provider.alpha
+            return layer
         }
     },
     computed: {
